724e61719c2592a4812830d39c3b62aa30ada24c
valeriialebedieva <valeriia.lebedieva1@gmail.com>
2025-09-04 11:55:03 +0200

    adde home page templates and layout

---
+++ public/js/script.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const footerText = document.querySelector(".copyright");
+    const year = new Date().getFullYear();
+    if (footerText) {
+        footerText.innerHTML = `&copy; ${year}, CSE 340 App`;
+    }
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const navToggle = document.getElementById("navToggle");
+    const navMenu = document.getElementById("navMenu");
+
+    navToggle.addEventListener("click", function () {
+        navMenu.classList.toggle("show");
+    });
+});
